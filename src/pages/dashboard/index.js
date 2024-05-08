39b527e137085a5eaa1b03bc15ffaa5e0dc3f00c
mateushedp <mateushedlundp@gmail.com>
2024-05-08 09:16:38 -0300

    refactor: added logout button

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,10 @@
+import { signOut } from "next-auth/react"
+
 export default function Dashboard() {
 	return (
-		<h2>dashboard</h2>
+		<div>
+			<h2>dashboard</h2>
+			<button onClick={() => signOut({ callbackUrl: "http://localhost:4000/login" })}>logout</button>
+		</div>
 	)
 }
