6e6f1b96fcda4caf9fc370c811ba802945a2c112
mateushedp <mateushedlundp@gmail.com>
2024-05-08 09:15:10 -0300

    feat: added title and favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
 	return (
 		<Html lang="en">
+			<title>VDuo</title>
+			<link rel="icon" href="/img/valorant_icon.png" sizes="any" />
+
 			<Head />
 			<body>
 				<Main />
