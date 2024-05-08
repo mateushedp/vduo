8e94ce2124c02d0e9a9ebd88d167c408dddbab85
mateushedp <mateushedlundp@gmail.com>
2024-05-08 08:57:12 -0300

    feat: added signin functionality

--- src/components/ui/socialsButton.js
+++ src/components/ui/socialsButton.js
@@ -3,10 +3,14 @@ import { signIn } from "next-auth/react"
 
 export default function SocialsButton({social, provider}) {
 	return (
-		<button className="flex items-center justify-start w-full border border-black rounded-sm py-3 px-5 hover:shadow-lg" onClick={() => signIn(provider, { callbackUrl: "/dashboard"})}>
+		<button className="flex items-center justify-start w-full border border-black rounded-sm py-3 px-5 hover:shadow-lg" onClick={() => {
+			if(provider){
+				signIn(provider, { callbackUrl: "/dashboard"})
+			}
+		}}>
 			<img src={`/img/socialIcons/${social}.png`} alt="" className="mr-2" />
 			<div className="flex-grow flex justify-center">
-				<p className="font-bold">Continue with {social}</p>
+				<p className="font-bold text-muted-foreground">Continue with {social}</p>
 			</div>
 		</button>
 	)
